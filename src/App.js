53ffb0f786f875609b79d9dccec12341f8618828
Togrul254 <togruliskenderov027@gmail.com>
2020-05-07 22:31:15 -0400

    aa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, } from 'react-router-dom';
-import Register from './Register';
-import Lostpassword from './Lostpassword';
-import Gps from './Gps';
-import About from './About';
-import Contact from './Contact';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Container, Navbar, NavbarBrand, Nav,NavbarToggle,NavbarCollapse} from 'react-bootstrap';
 
 
 class App extends React.Component {
@@ -22,89 +17,24 @@ class App extends React.Component {
   render() {
 
     return (
-
-
       <Router>
-      
-      <div className="Container" >
+        <Container fluid={true} className="p-0">
 
-        <div className="box">
+          <Navbar className="border-bottom" expand="lg">
+            <Navbar.Brand >ddd</Navbar.Brand>
+             <Navbar.Toggle className="border" aria-controls="navbar-toggle" />
+             <Navbar.Collapse id="navbar-toggle">
+               <Nav className="ml-auto">
+                 <Link className="nav-link" to="/">Home</Link>
+                 <Link className="nav-link" to="/About">About</Link>
+                 <Link className="nav-link" to="/Contact">Contact</Link>
+               </Nav>
+             </Navbar.Collapse>
+          </Navbar>
 
-          <div className="top">
-            <div className="image">
-              <a href ="http://utigps.com/">
-              <img style={{ width: "162px", height: "41px", }} src="logo.png"></img>
-              </a>
-              <p style={{ color: "grey", fontSize:"14px"}}>Global GPS izləmə sistemi</p>
-            </div>
-
-          </div>
-          <div className="box2">
-            <div className="lang">
-              <img style={{ width: "17px", height: "11px", }} src="aze.png"></img>
-              <p id="az">AZE</p>
-            </div>
-            <p id="paragraf">E-poçt</p>
-            <input type="text"></input>
-            <p id="paragraf">Şifrə</p>
-            <input type="Password"></input>
-            <button className="btn">Daxil ol</button>
-
-            <div className="prg2">
-
-          
-            <Link to={"/lostpassword"} id="paragraf2" > Şifrəni unutdun?</Link>
-            <Link to={"/register"} id="paragraf2" > Qeydiyyat</Link>
-            
-            </div>
-          </div>
-           
-           <div className="footer">
-             
-                  
-           <Link to={"/about"} id="paragraf3" > Haqqımızda</Link>
-           <Link to={"/contact"} id="paragraf3"> Əlaqə</Link>
-           <Link to={"/gps"} id="paragraf3" > GPS al</Link>
-       
-            
-
-           </div>
-     <div className="logo">
-       <a  href="https://itunes.apple.com/us/app/utigps/id1449786042?l=ru&amp;ls=1&amp;mt=8">
-       <img style={{margin:"5px"}} src="ios.png"></img>
-       </a>
-     
-       <a  href="https://play.google.com/store/apps/details?id=com.utigps">
-       <img style={{margin:"6px"}} src="android.png"></img>
-       </a>
-     </div>
-
-        </div> 
-
-      </div>
-      <Switch>
-
-    
-              <Route exact path="/lostpassword">
-                <Lostpassword/>
-              </Route>
-              <Route exact path="/register">
-                <Register/>
-              </Route>
-              <Route exact path="/about">
-                <About/>
-              </Route>
-              <Route exact path="/contact">
-                <Contact/>
-              </Route>
-              <Route exact path="/gps">
-                <Gps/>
-              </Route>
-      </Switch>
-           
+        </Container>
       </Router>
     );
   }
 }
 export default App;
-
