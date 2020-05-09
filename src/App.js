2ca280eaae8b24e82dd50c3cec763afba2df3aeb
Togrul254 <togruliskenderov027@gmail.com>
2020-05-08 23:47:27 -0400

    aaa

--- src/App.js
+++ src/App.js
@@ -15,19 +15,20 @@ class App extends React.Component {
       <Router>
         <Container fluid={true} className="p-0">
 
-          <Navbar className="border-bottom bg-dark" expand="lg">
+          <Navbar className="border-bottom bg-dark " expand="lg">
             <Navbar.Brand className=" text-info" >Navbar</Navbar.Brand>
             <Navbar.Toggle className="border" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto mt-2  ">
+              <Nav className="ml-auto"> 
                 <Form inline>
-                  <FormControl  type="text" placeholder="Search" />
-                  <Button type="button" size="sm" className=" m-0 ml-sm-2 mt-0" variant="outline-info" >Submit</Button>
+                  <FormControl style={{ height:"30px", marginTop:"5px"}} type="text" placeholder="Search" />
+                  <Button style={{ height:"30px",width:"60px"}} type="button" size="sm" className=" m-0 mt-1 ml-sm-2" variant="outline-info" >Submit</Button>
                 </Form>
                 <Link className="nav-link  text-info" to="/">Home</Link>
                 <Link className="nav-link  text-info" to="/About">About</Link>
                 <Link className="nav-link  text-info" to="/Contact">Contact</Link>
-
+ 
+ 
               </Nav>
             </Navbar.Collapse>
           </Navbar>
