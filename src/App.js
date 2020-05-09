1220712688ba0cf2c196573bbb0766973135ef77
Togrul254 <togruliskenderov027@gmail.com>
2020-05-09 07:08:06 -0400

    aaa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { Container, Navbar, NavbarBrand, Nav, NavbarToggle, NavbarCollapse, FormControl, Button, Form } from 'react-bootstrap';
+import { Container, Navbar, NavbarBrand, Nav, NavbarToggle, NavbarCollapse, FormControl, Button, Form, Col, Image } from 'react-bootstrap';
 
 
 class App extends React.Component {
@@ -16,20 +16,23 @@ class App extends React.Component {
         <Container fluid={true} className="p-0">
 
           <Navbar className="border-bottom bg-dark " expand="lg">
-            <Navbar.Brand className=" text-info" >Navbar</Navbar.Brand>
+          
+              
+            <Image  style={{ height: "70px", width: "180px" }} src="ll.png" />
+         
             <Navbar.Toggle className="border" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto"> 
-                <Form inline>
-                  <FormControl style={{ height:"30px", marginTop:"5px"}} type="text" placeholder="Search" />
-                  <Button style={{ height:"30px",width:"60px"}} type="button" size="sm" className=" m-0 mt-1 ml-sm-2" variant="outline-info" >Submit</Button>
-                </Form>
-                <Link className="nav-link  text-info" to="/">Home</Link>
-                <Link className="nav-link  text-info" to="/About">About</Link>
-                <Link className="nav-link  text-info" to="/Contact">Contact</Link>
- 
- 
+
+              <Nav>
+                <Link className="nav-link  text-warning " to="/">Home</Link>
+                <Link className="nav-link  text-warning" to="/About">About</Link>
+                <Link className="nav-link  text-warning" to="/Contact">Contact</Link>
               </Nav>
+
+              <Form inline className="ml-auto">
+                <FormControl style={{ height: "30px", marginTop: "5px" }} type="text" placeholder="Search" />
+                <Button style={{ height: "40px", width: "60px",  marginTop: "5px"}} type="button" size="sm" className=" m-0 mt-1 ml-sm-2" variant="outline-warning" >Submit</Button>
+              </Form>
             </Navbar.Collapse>
           </Navbar>
 
