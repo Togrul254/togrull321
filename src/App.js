168b8d145a66cfb7502eac8f3dc86e4981365de6
Togrul254 <togruliskenderov027@gmail.com>
2020-05-08 22:17:22 -0400

    aa

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { Container, Navbar, NavbarBrand, Nav,NavbarToggle,NavbarCollapse} from 'react-bootstrap';
+import { Container, Navbar, NavbarBrand, Nav, NavbarToggle, NavbarCollapse, FormControl, Button, Form } from 'react-bootstrap';
 
 
 class App extends React.Component {
 
-  
+
 
   render() {
 
@@ -15,21 +15,26 @@ class App extends React.Component {
       <Router>
         <Container fluid={true} className="p-0">
 
-          <Navbar className="border-bottom" expand="lg">
-            <Navbar.Brand >Navbar</Navbar.Brand>
-             <Navbar.Toggle className="border" aria-controls="navbar-toggle" />
-             <Navbar.Collapse id="navbar-toggle">
-               <Nav className="ml-auto">
-                 <Link className="nav-link" to="/">Home</Link>
-                 <Link className="nav-link" to="/About">About</Link>
-                 <Link className="nav-link" to="/Contact">Contact</Link>
-               </Nav>
-             </Navbar.Collapse>
+          <Navbar className="border-bottom bg-dark" expand="lg">
+            <Navbar.Brand className=" text-info" >Navbar</Navbar.Brand>
+            <Navbar.Toggle className="border" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto mt-2  ">
+                <Form inline>
+                  <FormControl  type="text" placeholder="Search" />
+                  <Button type="button" size="sm" className=" m-0 ml-sm-2 mt-0" variant="outline-info" >Submit</Button>
+                </Form>
+                <Link className="nav-link  text-info" to="/">Home</Link>
+                <Link className="nav-link  text-info" to="/About">About</Link>
+                <Link className="nav-link  text-info" to="/Contact">Contact</Link>
+
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
-d
+
         </Container>
       </Router>
-      
+
     );
   }
 }
